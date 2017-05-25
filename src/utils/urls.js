d08acfdc6383382daa91297ac2c3cf72898643c9
Krzysztof Gołębiowski <krzysiek.golebiowski@gmail.com>
2017-05-25 12:26:27 +0200

    Step 2

--- src/utils/urls.js
+++ src/utils/urls.js
@@ -1,11 +1,10 @@
 export function buildQueryString(params) {
-  return Object.keys(params)
-    .map((key) => [key, params[key]])
-    .map((pair) => pair.map(encodeURIComponent))
-    .map((pair) => pair.join('='))
-    .join('&');
+    return Object.keys(params)
+        .map(function (key) { return [key, params[key]]; })
+        .map(function (pair) { return pair.map(encodeURIComponent); })
+        .map(function (pair) { return pair.join('='); })
+        .join('&');
 }
-
 export function buildUrl(baseUrl, params) {
-  return `${baseUrl}?${buildQueryString(params)}`;
+    return baseUrl + "?" + buildQueryString(params);
 }
---
+++ src/utils/urls.ts
@@ -0,0 +1,11 @@
+export function buildQueryString(params: object): string {
+  return Object.keys(params)
+      .map((key) => [key, params[key]])
+      .map((pair) => pair.map(encodeURIComponent))
+      .map((pair) => pair.join('='))
+      .join('&');
+}
+
+export function buildUrl(baseUrl: string, params: object): string {
+  return `${baseUrl}?${buildQueryString(params)}`;
+}
